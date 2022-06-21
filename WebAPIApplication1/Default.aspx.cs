e5ca2ad2117a2f731ee05a8986ddbc15490ddb17
Shruti1118 <padwal.shruti.12ee1035@gmail.com>
2022-06-22 00:29:56 +0530

    Add project files.

---
+++ WebAPIApplication1/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace WebAPIApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string strurltest = String.Format("https://jsonplaceholder.typicode.com/posts/1/comments");
+            WebRequest requestObjGet = WebRequest.Create(strurltest);
+            requestObjGet.Method = "Get";
+            HttpWebResponse responseObjGet = null;
+            responseObjGet = (HttpWebResponse)requestObjGet.GetResponse();
+
+            string strresulttest = null;
+            using(Stream stream = responseObjGet.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                strresulttest = sr.ReadToEnd();
+                sr.Close();
+            }
+
+
+            //Post
+            String strurl = String.Format("https://jsonplaceholder.typicode.com/posts");
+            WebRequest requestObjPost = WebRequest.Create(strurl);
+            requestObjPost.Method = "Post";
+            requestObjPost.ContentType = "application/json";
+            string postData = "{\"title\":\"testdata\",\"body\":\"testbody\",\"userId\":\"50\"}";
+
+            using (var streamWriter = new StreamWriter(requestObjPost.GetRequestStream()))
+            {
+                streamWriter.Write(postData);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                var httpResponse = (HttpWebResponse)requestObjPost.GetResponse();
+
+                using(var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result2 = streamReader.ReadToEnd();
+
+                }
+            }
+
+        }
+    }
+}
